a7bebb6dd789b210223b9602521421459188d238
Darcy Price <darcyelsing@gmail.com>
2020-12-02 13:47:43 +1100

    WIP

--- main.js
+++ main.js
@@ -1,158 +1,3 @@
-/**
- * JSON-RPC Client Exception class
- *
- * @param String code
- * @param String message
- */
-var JSONRpcClientException = function (code, message) {
-	this.code = code;
-	this.message = message;
-};
-JSONRpcClientException.prototype = jQuery.extend(
-	JSONRpcClientException.prototype,
-	{
-		/**
-		 * Magic method. COnvert object to string.
-		 *
-		 * @return String
-		 */
-		toString: function () {
-			return '[' + this.code + '] ' + this.message;
-		},
-	}
-);
-
-/**
- * JSON-RPC Client
- *
- * @param Object options
- */
-var JSONRpcClient = function (options) {
-	this.setOptions(options);
-	this.init();
-};
-JSONRpcClient.prototype = jQuery.extend(JSONRpcClient.prototype, {
-	/**
-	 * Default options
-	 */
-	options: {
-		onerror: function () {},
-		onsuccess: function () {},
-		url: '',
-		headers: {},
-	},
-	current: 1,
-	onerror: null,
-	onsuccess: null,
-	onstart: null,
-
-	/**
-	 * Init client
-	 */
-	init: function () {
-		this.onerror = this.getParam('onerror');
-		this.onsuccess = this.getParam('onsuccess');
-
-		this.initMethods();
-	},
-
-	/**
-	 * Init API methiods by url
-	 */
-	initMethods: function () {
-		var instance = this;
-		// get all methods
-		jQuery.ajax(this.getParam('url'), {
-			async: false,
-			success: function (data) {
-				if (data.methods) {
-					// create method
-					jQuery.each(data.methods, function (methodName, methodParams) {
-						var method = function () {
-							var params = new Array();
-							for (var i = 0; i < arguments.length; i++) {
-								params.push(arguments[i]);
-							}
-							var id = instance.current++;
-							var callback = params[params.length - 1];
-							var request = {
-								jsonrpc: '2.0',
-								method: methodName,
-								params: params,
-								id: id,
-							};
-
-							var async = false;
-							if (jQuery.type(callback) == 'function') {
-								async = true;
-								params.pop();
-							}
-
-							var res = null;
-							// API request
-							jQuery.ajax(instance.getParam('url'), {
-								contentType: 'application/json',
-								type: methodParams.transport,
-								processData: false,
-								dataType: 'json',
-								cache: false,
-								data: JSON.stringify(request),
-								headers: instance.getParam('headers'),
-								async: async,
-								success: function (result) {
-									if (jQuery.type(result.error) == 'object') {
-										res = new JSONRpcClientException(
-											result.error.code,
-											result.error.message
-										);
-										instance.onerror(res);
-									} else {
-										res = result.result;
-										if (jQuery.type(callback) == 'function') {
-											callback(res);
-										}
-									}
-									instance.onsuccess(res, id, methodName);
-								},
-							});
-							if (!async) {
-								return res;
-							}
-						};
-
-						instance[methodName] = method;
-					});
-				} else {
-					throw Exception('Methods could not be found');
-				}
-			},
-		});
-	},
-
-	/**
-	 * Set client options
-	 *
-	 * @param Object options
-	 */
-	setOptions: function (options) {
-		this.options = jQuery.extend({}, this.options, options);
-	},
-
-	/**
-	 * Get client param, if param is not available in this.options return defaultValue
-	 *
-	 * @param String key
-	 * @param mixed defaultValue
-	 * @return mixed
-	 */
-	getParam: function (key, defaultValue) {
-		if (jQuery.type(this.options[key]) != 'undefined') {
-			return this.options[key];
-		}
-		return defaultValue;
-	},
-});
-
 jQuery(document).ready(function () {
 	Date.prototype.format = function () {
 		return (
@@ -444,8 +289,6 @@ jQuery(document).ready(function () {
 
 			const datetime = `${this.date} ${this.time}`;
 
-			console.log(datetime);
-
 			this.client.calculateEndTime(
 				datetime,
 				this.eventId,
@@ -463,12 +306,9 @@ jQuery(document).ready(function () {
 					const formattedLessonDate = dayjs(instance.date).format(
 						'dddd DD MMMM YYYY'
 					);
-
-					const formattedLessonStartTime = dayjs(instance.time).format(
-						(hh: MMa)
-					);
-					const formattedLessonEndTime = dayjs(res).format((hh: MMa));
-					const formattedTime = `${formattedLessonStartTime}-${formattedLessonEndTime}`;
+					const formattedLessonStartTime = dayjs(datetime).format('hh:MMa');
+					const formattedLessonEndTime = dayjs(res).format('hh:MMa');
+					const formattedTime = `${formattedLessonStartTime} - ${formattedLessonEndTime}`;
 
 					jQuery('#lesson-date').text(formattedLessonDate);
 					jQuery('#lesson-time').text(formattedTime);
@@ -545,9 +385,6 @@ jQuery(document).ready(function () {
 								const formattedStartTime = dayjs(`${date} ${startTime}`).format(
 									'h:mm a'
 								);
-
-								console.log(typeof startTime);
-
 								const newDiv = `<div class="time-item" data-time="${startTime}" onclick="handleTimeItemClick()">${formattedStartTime}</div>`;
 								jQuery('#time').append(newDiv);
 							}
